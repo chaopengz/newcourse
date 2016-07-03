9d94cea63ed959e8664fc515ca0f7f25f092c7fc
lykeven <wangyanzxyq@yahoo.com>
2016-07-03 09:45:15 +0800

    add initial model

--- newcourse/app/models.py
+++ newcourse/app/models.py
@@ -2,4 +2,79 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    is_admin = models.BooleanField()
+
+
+class Term(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField()
+    week = models.IntegerField()
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=30)
+    is_allowed = models.BooleanField()
+    max_number = models.IntegerField()
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=30)
+    introduction = models.CharField(max_length=300)
+    is_single = models.BooleanField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    term = models.ForeignKey('Term')
+
+
+class Resource(models.Model):
+    name = models.CharField(max_length=30)
+    directory = models.CharField(max_length=500)
+    server_path = models.FilePathField()
+    course = models.ForeignKey('Course')
+
+
+class Message(models.Model):
+    content = models.CharField(max_length=300)
+    publish_date = models.DateField()
+    course = models.ForeignKey('Course')
+    user = models.ForeignKey('User')
+
+
+class TaskRequirement(models.Model):
+    base_requirements = models.CharField(max_length=300)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_single = models.BooleanField()
+    course = models.ForeignKey('Course')
+
+
+class TaskFile(models.Model):
+    name = models.CharField(max_length=300)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    content = models.CharField(max_length=300)
+    task_requirement = models.ForeignKey('TaskRequirement')
+    user = models.ForeignKey('User')
+    group_id = models.IntegerField()
+
+
+class UserCourse(models.Model):
+    user = models.ForeignKey('User')
+    course = models.ForeignKey('Course')
+
+
+class UserGroup(models.Model):
+    user = models.ForeignKey('User')
+    group = models.ForeignKey('Group')
+    is_allowed = models.BooleanField()
+
+
+class GroupCourse(models.Model):
+    group = models.ForeignKey('Group')
+    course = models.ForeignKey('Course')
+    is_allowed = models.BooleanField()
--- newcourse/newcourse/settings.py
+++ newcourse/newcourse/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'newcourse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'newcourse',
+        'USER':'buaa',
+        'PASSWORD':'buaa',
+        'HOST':'newcourse.online',
+        'PORT':'',
     }
 }
 
