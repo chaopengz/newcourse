caf14883bee11f880633e8eb941e6e8f944b1aa3
LiuKaiXin <254227712@qq.com>
2016-07-04 08:36:35 +0900

    完成用户故事2（管理员维护学期信息）

--- newcourse/app/jiaowu.py
+++ newcourse/app/jiaowu.py
@@ -28,7 +28,7 @@ def modifyTerm(request):
         term.start_date =start_y + '-' + start_m + '-' + start_d
         term.end_date = end_y + '-' + end_m + '-' + end_d
         term.save()
-        return HttpResponse(start)
+        return render_to_response('jiaowu_term_success.html')
     else:
         list_num = 1
         page_name = '学期管理'
