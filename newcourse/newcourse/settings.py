9c5e0c605cba4a26c8fbfa2badfbe3da69adaffa
LiuKaiXin <254227712@qq.com>
2016-07-04 02:48:56 +0900

    add jiaowu.py

--- newcourse/newcourse/settings.py
+++ newcourse/newcourse/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates'),
-                 'E:/myproject/python/newcourse/newcourse/app/templates']
+                 'D:/KeJian/ShengChanShiXi/newcourse/newcourse/app/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
